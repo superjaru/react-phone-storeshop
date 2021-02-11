453ee1bf96380a73daf77f82913f728f4dd43584
superjaru <another_jaru@hotmail.com>
2021-02-11 16:44:41 +0700

    paypal button added

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import Cart from "./components/Cart/Cart";
 import Default from "./components/Default";
 import Modal from "./components/Modal";
 
-// zabaaaaaaaaaaa
 function App() {
   return (
     <Fragment>
       <Navbar />
       <Switch>
         {/* มี exact เพราะต้องการให้ path เป๊ะๆ ถ้าไม่ใส่ path="/" คือเอาทุกแบบ ดังนั้นมันจะ route ไปอันอื่นไม่ได้นอกจะย้ายบรรทัดมันไปอยู่ล่างสุด ซึ่งมันค่อนข้าง  brute force */}
+        {/* ในแต่ละ route มีการ pass props ไปให้ด้วย */}
         <Route exact path="/" component={ProductList} />
         <Route path="/details" component={Details} />
         <Route path="/cart" component={Cart} />
--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PayPalButton from "./PayPalButton";
 
-export default function CartTotals({ value }) {
+export default function CartTotals({ value, history }) {
   const { cartSubtotal, cartTax, cartTotal, clearCart } = value;
   return (
     <React.Fragment>
@@ -9,32 +10,31 @@ export default function CartTotals({ value }) {
         <div className="row">
           <div className="col-10 mt-2 ml-sm-5 ml-md-auto  col-sm-8 text-right text-capitalize">
             <Link to="/">
-            <button
-              className="btn btn-outline-danger text-uppercase mb-3 px-5"
-              type="button"
-              onClick={clearCart}
-            >
-              clear cart
-            </button>
+              <button
+                className="btn btn-outline-danger text-uppercase mb-3 px-5"
+                type="button"
+                onClick={clearCart}
+              >
+                clear cart
+              </button>
             </Link>
             <h5>
-              <span className="text-title">
-                subtotal :
-              </span>
+              <span className="text-title">subtotal :</span>
               <strong>$ {cartSubtotal}</strong>
-              </h5>
-              <h5>
-              <span className="text-title">
-                tax :
-              </span>
+            </h5>
+            <h5>
+              <span className="text-title">tax :</span>
               <strong>$ {cartTax}</strong>
-              </h5>
-              <h5>
-              <span className="text-title">
-                total :
-              </span>
+            </h5>
+            <h5>
+              <span className="text-title">total :</span>
               <strong>$ {cartTotal}</strong>
             </h5>
+            <PayPalButton
+              total={cartTotal}
+              clearCart={clearCart}
+              history={history}
+            />
           </div>
         </div>
       </div>
